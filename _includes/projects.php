2a2ca12ef667c31493857e81ae5159c6bc7b80bd
EdwinGameDev <edwinjonesholanda@gmail.com>
2024-02-15 22:21:52 +0000

    Update projects section

--- _includes/projects.php
+++ _includes/projects.php
@@ -2,16 +2,13 @@
     <section class="section projects" id="projects">
         <div class="full-width">
             <div class="section-heading" id="featured-projects-text">Projects</div>
-            
-                <div class="project-technologies">
-
-                    <a id="featured" class="semi-transparent-button with-border white-button" onclick="selected('featured')">Featured</a>
-
-                </div>
-
-            <div class="projects-wrapper">
-            <!-- filled in by js -->
-            </div>
+            <div class="project-technologies">
+                <p>
+                    Coming soon!
+                </p>
+                <!--<a id="featured" class="semi-transparent-button with-border white-button" onclick="selected('featured')">Featured</a>-->
+            </div>          
+            <div class="projects-wrapper"> </div>          
         </div>
     </section>
 </div>
